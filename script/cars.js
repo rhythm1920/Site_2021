4cb38c66ddc2c849b0dc0e966c4a1ff1374a3115
Sid250101 <sid25porwal@gmail.com>
2021-03-21 13:25:28 +0530

    changes discussed on 20-03-2021

--- script/cars.js
+++ script/cars.js
@@ -16,7 +16,7 @@ cars.forEach((car) => {
     $(`${car} > .flip-card-base`).toggleClass("flipper");
   });
 
-  $(`${car} .car-image`).click(() => {
+  $(`${car} .car-image`).hover(() => {
     $(`${car} .car-actual-image`).toggleClass("show-car");
   });
 });
