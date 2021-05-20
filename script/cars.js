701c53b294c608a57615fc2669c1dc9d3f353367
Sid250101 <sid25porwal@gmail.com>
2021-05-20 20:20:47 +0530

    final changes v2, completed all known issue

--- script/cars.js
+++ script/cars.js
@@ -1,10 +1,3 @@
-// $(".front").click(() => {
-//   $(".flip-card-base").addClass("turner");
-// });
-
-// $(".back").click(() => {
-//   $(".flip-card-base").removeClass("turner");
-// });
 cars = ["#car_1", "#car_2", "#car_3", "#car_4", "#car_5", "#car_6", "#car_7"];
 
 cars.forEach((car) => {
