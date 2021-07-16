c38501a1d0d18534d061466538db3d48d0ef868e
Sid250101 <sid25porwal@gmail.com>
2021-07-16 23:17:01 +0530

    fixed google notes issue on 16/07/21

--- script/cars.js
+++ script/cars.js
@@ -1,7 +1,7 @@
 cars = ["#car_1", "#car_2", "#car_3", "#car_4", "#car_5", "#car_6", "#car_7"];
 
 cars.forEach((car) => {
-  $(`${car}_text`).click(() => {
+  $(`${car} .front`).click(() => {
     $(`${car} > .flip-card-base`).toggleClass("flipper");
   });
 
