2d72c15f2812cd8ddd62364dd374f1a08defe477
KARTkgp <43110269+KARTkgp@users.noreply.github.com>
2022-04-23 22:36:16 +0530

    Update cars.js

--- script/cars.js
+++ script/cars.js
@@ -1,4 +1,4 @@
-cars = ["#car_1", "#car_2", "#car_3", "#car_4", "#car_5", "#car_6", "#car_7"];
+cars = ["#car_1", "#car_2", "#car_3", "#car_4", "#car_5", "#car_6", "#car_7", "#car_8"];
 
 cars.forEach((car) => {
   $(`${car} .front`).click(() => {
